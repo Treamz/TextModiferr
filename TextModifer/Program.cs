66e1c1a82c6c1c67fcae798f4ed07ee6bcf7e95c
Treamz <avtoritet1337@gmail.com>
2018-03-22 17:55:54 +0300

    1

---
+++ TextModifer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using MoreLinq;
+
+namespace TextModifer
+{
+
+    class Program
+    {
+
+
+
+    public static String path = @"F:\1.txt";
+    public static String path2 = @"F:\2.txt";
+        static void Main(string[] args)
+        {
+
+            // Delete blank lines from file
+            // Delete blank lines from file
+            var lines = System.IO.File.ReadAllLines(path).Where(arg => !string.IsNullOrWhiteSpace(arg));
+            System.IO.File.WriteAllLines(path, lines);
+          
+
+            var stringsPairs = File.ReadLines(path).Batch(2).Select(p => string.Join("\n @ " , p));
+
+            File.WriteAllLines(path2, stringsPairs, Encoding.UTF8);
+
+        }
+
+
+
+        static void oldMain()
+        {
+
+
+        }
+
+        public static void Linecount()
+        {
+            int count = System.IO.File.ReadAllLines(path).Length;
+            Console.WriteLine(count);
+        }
+
+        public static void Chetnie()
+        {
+            string[] lines = File.ReadAllLines(path);
+            int n = 1;
+            while (n < lines.Length)
+            {
+                Console.WriteLine(lines[n]);
+                n += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TextModifer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TextModifer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextModifer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e573902f-9d13-42ac-9013-11f0bc99e5af")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
